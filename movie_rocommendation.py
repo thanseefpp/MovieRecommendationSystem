d13f024d4ffe577ee947df971676fbcdafa16ef3
Thanseef <androtsf@gmail.com>
2023-01-08 00:26:47 +0530

    streamlit issues fixed and result function generated

--- movie_rocommendation.py
+++ movie_rocommendation.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import difflib # using to get the close matches
-from sklearn.feature_extraction.text import TfidfVectorizer # to transform the text data to numerical feature vector.
+import difflib  # using to get the close matches
+# to transform the text data to numerical feature vector.
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
 
@@ -8,15 +9,16 @@ import streamlit as st
 movies_data = pd.read_csv('Data/movies.csv')
 
 # selecting the relevant features for the recommendation
-selected_features = ['genres','keywords','tagline','cast','director']
+selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
 
 # replacing the null values with null string to avoid error.
 
 for features in selected_features:
-  movies_data[features] = movies_data[features].fillna("")
-  
+    movies_data[features] = movies_data[features].fillna("")
+
 # combining all the 5 selected features
-combined_features = movies_data['genres']+' '+movies_data['keywords']+' '+movies_data['tagline']+' '+movies_data['cast']+' '+movies_data['director']
+combined_features = movies_data['genres']+' '+movies_data['keywords']+' ' + \
+    movies_data['tagline']+' '+movies_data['cast']+' '+movies_data['director']
 
 # converting the text data to numerical data
 vectorizer = TfidfVectorizer()
@@ -28,28 +30,39 @@ similarity = cosine_similarity(feature_vectors)
 st.title("Movie Recommendation System")
 
 movie_name = st.text_input("Enter your favorite movie name : ")
+predict_result = ""
 
-# movie_name = input("Enter your favorite movie name : ")
+if st.button("Get Result"):
+    list_all_movie_titles = movies_data['title'].tolist()
 
-list_all_movie_titles = movies_data['title'].tolist()
+    find_close_match = difflib.get_close_matches(
+        movie_name, list_all_movie_titles)
 
-find_close_match = difflib.get_close_matches(movie_name,list_all_movie_titles)
+    close_match = find_close_match[0]
 
-close_match = find_close_match[0]
+    index_of_the_movie = movies_data[movies_data.title ==
+                                     close_match]['index'].values[0]
 
-index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
+    similarity_score = list(enumerate(similarity[index_of_the_movie]))
 
-similarity_score = list(enumerate(similarity[index_of_the_movie]))
+    sorted_similarity_movies = sorted(
+        similarity_score, key=lambda x: x[1], reverse=True)
+    dictionary = {}
+    # print("Suggested Movies for you :\n")
+    def recommend_movie_names():
+        i = 1
+        
+        for movie in sorted_similarity_movies:
+            index = movie[0]  # to get the index value
+            title_from_index = movies_data[movies_data.index == index]['title'].values[0]
+            if (i <= 20):
+                collection = {i: title_from_index}
+                dictionary.update(collection)
+                
+                i += 1
 
-sorted_similarity_movies = sorted(similarity_score, key= lambda x:x[1], reverse=True)
-
-print("Suggested Movies for you :\n")
-
-i = 1
-
-for movie in sorted_similarity_movies:
-  index = movie[0] #to get the index value
-  title_from_index = movies_data[movies_data.index==index]['title'].values[0]
-  if (i<=20):
-    print(f"{i} -> {title_from_index}")
-    i+=1
+    recommend_movie_names()
+    
+    for item in range(1,len(dictionary)):
+        st.write(str(item) + '. ' + str(dictionary[item])) 
+                
